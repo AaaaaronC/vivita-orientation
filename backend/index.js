edcfd7f628b846f3297fd9bc3de0c74b83fbb5a3
AaronC <acz2606@gmail.com>
2023-02-07 16:47:58 +0800

    test: register

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,11 @@ const bodyParser = require("body-parser");
 //Enable mysql
 const mysql = require("mysql");
 
-const CLIENT_URL = "http://localhost:3000";
-const BACKEND_PORT = 3001;
+// const CLIENT_URL = 'http://localhost:3000';
+const BACKEND_URL = "http://localhost:3001";
+
+const CLIENT_URL = "http://18.141.207.124";
+// const BACKEND_URL = "http://18.141.207.124";
 
 app.use(
   cors({
