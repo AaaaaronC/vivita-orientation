c6b445f97c75fefd10727884926e1336bf2d191d
AaronC <acz2606@gmail.com>
2023-02-07 17:01:41 +0800

    test: register

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,10 @@ import { useEffect, useState } from "react";
 import Axios from "axios";
 
 // const CLIENT_URL = 'http://localhost:3000';
-// const BACKEND_URL = 'http://localhost:3001';
+// const BACKEND_URL = "http://localhost:3001";
 
-const CLIENT_URL = "http://18.141.207.124/";
-const BACKEND_URL = "http://18.141.207.124/api";
+const CLIENT_URL = "http://18.141.207.124";
+const BACKEND_URL = "http://18.141.207.124";
 
 function App() {
   const [email, setEmail] = useState(null);
