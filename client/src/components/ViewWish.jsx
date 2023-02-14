bf8b4e9ff5bc21b96fb9b845cda9cf018ea497cb
AaronC <acz2606@gmail.com>
2023-02-14 17:14:28 +0800

    add: check

--- client/src/components/ViewWish.jsx
+++ client/src/components/ViewWish.jsx
@@ -3,7 +3,7 @@ import Axios from "axios";
 import { Link } from "react-router-dom";
 
 function ViewWish(wish) {
-  
+
   const backHandler = () => {
     window.location.reload();
   };
@@ -11,9 +11,9 @@ function ViewWish(wish) {
   return (
     <div className="viewWishOverlay">
       <div className="viewWishWrapper">
-        <h1 className="wishTitle">{wish.title}</h1>
-        <h4 className="wishPoster">{wish.email}</h4>
-        <h4 className="wishBody">{wish.body}</h4>
+        <h1 className="viewWishTitle">{wish.title}</h1>
+        <h4 className="viewWishPoster">{wish.email}</h4>
+        <h4 className="viewWishBody">{wish.body}</h4>
         <Link className="viewWishButton" onClick={backHandler}>Back</Link>
       </div>
     </div>
