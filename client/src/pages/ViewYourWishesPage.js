287b3ba42c4fe7aa08e8258ca386d7b9ec4ac6ad
AaronC <acz2606@gmail.com>
2023-02-15 16:59:18 +0800

    test

--- client/src/pages/ViewYourWishesPage.js
+++ client/src/pages/ViewYourWishesPage.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 import { React, useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Delete from "../images/delete.png";
 import Edit from "../images/edit.png";
 
